a0d02f8b4292d41441511b1017328aebe50b0ae9
Lodewijk Wensveen <lodewijkwensveen@protonmail.com>
2021-08-02 14:21:24 +0200

    Added bpmn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiagramComponent } from './diagram/diagram.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiagramComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
